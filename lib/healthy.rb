bce8586eb69b1524db7a4eac996aa60cc32985b6
Drew Blas <drew.blas@gmail.com>
2011-09-01 11:33:30 -0500

    Added load_path that does not cause the healthy gem to find the wrong file

--- lib/healthy.rb
+++ lib/healthy.rb
@@ -1,5 +1,7 @@
 DIAGNOSTIC_LOGGER = defined?(Rails) ? Rails.logger : require('logger') && Logger.new($stdout)
 
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'diagnostic'
 require 'base'
 require 'sinatra'
