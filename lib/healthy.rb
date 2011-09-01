ce6083d3784141928b7435cdc66cb0ef5db616b5
Drew Blas <drew.blas@gmail.com>
2011-09-01 11:43:31 -0500

    Re-Packaged healthy according to:
    http://weblog.rubyonrails.org/2009/9/1/gem-packaging-best-practices
    
    This addressed the load path issue without overriding the load_path

--- lib/healthy.rb
+++ lib/healthy.rb
@@ -1,14 +1,12 @@
 DIAGNOSTIC_LOGGER = defined?(Rails) ? Rails.logger : require('logger') && Logger.new($stdout)
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-
-require 'diagnostic'
-require 'base'
+require 'healthy/diagnostic'
+require 'healthy/base'
 require 'sinatra'
-require 'server'
-require 'server_identity'
-require 'router'
-require('gem_list')          && Healthy::Diagnostic.monitor(Healthy::GemList)  
-require('env_check')         && Healthy::Diagnostic.monitor(Healthy::EnvCheck)  
-require('disk_space')        && Healthy::Diagnostic.monitor(Healthy::DiskSpace)
-require('revision_deployed') && Healthy::Diagnostic.monitor(Healthy::RevisionDeployed)
+require 'healthy/server'
+require 'healthy/server_identity'
+require 'healthy/router'
+require('healthy/gem_list')          && Healthy::Diagnostic.monitor(Healthy::GemList)  
+require('healthy/env_check')         && Healthy::Diagnostic.monitor(Healthy::EnvCheck)  
+require('healthy/disk_space')        && Healthy::Diagnostic.monitor(Healthy::DiskSpace)
+require('healthy/revision_deployed') && Healthy::Diagnostic.monitor(Healthy::RevisionDeployed)
